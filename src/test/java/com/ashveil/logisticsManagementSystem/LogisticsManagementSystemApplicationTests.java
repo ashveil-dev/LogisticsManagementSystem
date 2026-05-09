61b7ef1481a3dca3744100b0ace2d9d09947194a
ashveil <ashveil.dev@gmail.com>
2026-05-10 07:05:51 +0900

    @Set 프로젝트 초기 세팅하기

---
+++ src/test/java/com/ashveil/logisticsManagementSystem/LogisticsManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashveil.logisticsManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
