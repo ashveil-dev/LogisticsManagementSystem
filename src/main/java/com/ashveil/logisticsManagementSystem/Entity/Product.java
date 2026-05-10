e2faa855170c4282fb92ac381de454eaca81907e
ashveil <ashveil.dev@gmail.com>
2026-05-10 10:23:35 +0900

    @Feat ProductEntity에 Builder 어노테이션 추가하기

--- src/main/java/com/ashveil/logisticsManagementSystem/Entity/Product.java
+++ src/main/java/com/ashveil/logisticsManagementSystem/Entity/Product.java
@@ -4,15 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.val;
+import lombok.*;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
