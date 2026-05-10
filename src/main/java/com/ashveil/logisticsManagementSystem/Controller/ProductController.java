79bea63c9c3c6b4b42491fb99b47f52155926495
ashveil <ashveil.dev@gmail.com>
2026-05-10 10:02:06 +0900

    @Feat 상품 정보를 수정할 수 있는 edit 함수를 ProductController에 추가하기

--- src/main/java/com/ashveil/logisticsManagementSystem/Controller/ProductController.java
+++ src/main/java/com/ashveil/logisticsManagementSystem/Controller/ProductController.java
@@ -1,12 +1,10 @@
 package com.ashveil.logisticsManagementSystem.Controller;
 
+import com.ashveil.logisticsManagementSystem.Dto.Request.EditProductRequestDto;
 import com.ashveil.logisticsManagementSystem.Entity.Product;
 import com.ashveil.logisticsManagementSystem.Repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,5 +19,30 @@ public class ProductController {
     @GetMapping("/product")
     public Object getAll() {
         return productRepository.findAll();
+    }
+
+    @PutMapping("/product/{id}")
+    public Product edit(@PathVariable Long id, @RequestBody EditProductRequestDto request) {
+        Product findProduct = productRepository
+                .findById(id)
+                .orElseThrow(() -> new RuntimeException("상품 없음"));
+
+        String name = request.getName();
+        Integer price = request.getPrice();
+        Integer stock = request.getStock();
+
+        if(name != null) {
+            findProduct.setName(name);
+        }
+
+        if(price != null) {
+            findProduct.setPrice(price);
+        }
+
+        if(stock != null) {
+            findProduct.setStock(stock);
+        }
+
+        return productRepository.save(findProduct);
     }
 }
